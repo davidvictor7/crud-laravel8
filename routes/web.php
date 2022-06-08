5842ba11a2243da47891cb463f812334d6b9e8d3
davidvictor7 <david.v.c.2013dd@gmail.com>
2022-06-07 22:26:38 -0300

    Route to list posts

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\{
+    PostController
+};
+
 use Illuminate\Support\Facades\Route;
+
+Route::get('/posts', [PostController::class, 'Index']);
 
 Route::get('/', function () {
     return view('welcome');
+
 });
