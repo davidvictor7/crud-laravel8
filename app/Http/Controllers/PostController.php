b63db094bc69eb32d30aaa3d1b4db7d16e0789f4
davidvictor7 <david.v.c.2013dd@gmail.com>
2022-06-19 20:21:10 -0300

    Função de criar o post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,8 +8,15 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function Index(){
-        $posts = Post::all();
+        $posts = Post::all()->sortByDesc('created_at');
         // dd($posts);
         return  view('admin.posts.index', compact('posts'));
+    }
+
+    public function criarPost(Request $request){
+        $titulo = $request->titulo;
+        $conteudo = $request->conteudo;
+       
+        return response()->json($request);
     }
 }
